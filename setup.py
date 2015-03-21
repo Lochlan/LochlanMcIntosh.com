2eba9f47ded50dd4af3430698bee4dbbf537dd16
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-03-21 02:10:21 -0700

    First commit, add jfmatth/openshift-django17

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='django 1.7 on Red Hat Openshift',
+    version='1.3',
+    description='django on OpenShift',
+    author='',
+    author_email='',
+    url='https://github.com/jfmatth/openshift-django17',
+)
