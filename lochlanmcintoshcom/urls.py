203f27125e70f994d040c736fa936c0df6a6bfee
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-03-21 15:19:44 -0700

    Replace index view with simple template view
    Add template based on frontend project
    Delete views.py

--- lochlanmcintoshcom/urls.py
+++ lochlanmcintoshcom/urls.py
@@ -1,18 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
-from lochlanmcintoshcom.views import Index
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'lochlanmcintoshcom.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', Index.as_view(), name='index'),
+    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
-
-
-
