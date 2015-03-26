c660e769a598c4d2e3447c7349a662855379fc43
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-03-25 17:18:50 -0700

    Add canonical URL

--- lochlanmcintoshcom/settings.py
+++ lochlanmcintoshcom/settings.py
@@ -125,3 +125,14 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, 'templates'),
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
