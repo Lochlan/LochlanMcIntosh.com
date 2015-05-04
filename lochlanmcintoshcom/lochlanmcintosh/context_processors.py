012fb4101942ae477d1b203f31de2148a7d19c21
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-05-03 22:27:56 -0700

    Move CSS/JS versioning to filename instead of GET string

--- lochlanmcintoshcom/lochlanmcintosh/context_processors.py
+++ lochlanmcintoshcom/lochlanmcintosh/context_processors.py
@@ -4,9 +4,12 @@ import time
 def environ(request):
     try:
         version_hash = os.environ['DEPLOYED_GIT_COMMIT_HASH']
+        dot_prefixed_version_hash = '.' + version_hash
     except KeyError:
-        version_hash = time.time()
+        version_hash = ''
+        dot_prefixed_version_hash = ''
 
     return {
+        'dot_prefixed_version_hash': dot_prefixed_version_hash,
         'version_hash': version_hash,
     }
