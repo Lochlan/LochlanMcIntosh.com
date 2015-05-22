46105d060092e7d21d826bc226c1881d9d609941
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-05-22 01:36:26 -0700

    Remove unused dictionaries from url patterns

--- lochlanmcintoshcom/urls.py
+++ lochlanmcintoshcom/urls.py
@@ -5,14 +5,10 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="shared/home.html"),
-            { 'included_template': 'home' }, name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name="shared/about.html"),
-            { 'included_template': 'about' }, name='about'),
-    url(r'^contact/$', TemplateView.as_view(template_name="shared/contact.html"),
-            { 'included_template': 'contact' }, name='contact'),
-    url(r'^resume/$', TemplateView.as_view(template_name="shared/resume.html"),
-            { 'included_template': 'resume' }, name='resume'),
+    url(r'^$', TemplateView.as_view(template_name="shared/home.html"), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name="shared/about.html"), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name="shared/contact.html"), name='contact'),
+    url(r'^resume/$', TemplateView.as_view(template_name="shared/resume.html"), name='resume'),
 
     url(r'^api/', include('lochlanmcintoshcom.api.urls')),
 
