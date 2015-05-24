c83bf5865a0ff8af5352c3367c38afbcfa006d8f
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-05-23 20:45:13 -0700

    Add more router unit tests

--- tests/js/routerSpec.js
+++ tests/js/routerSpec.js
@@ -11,7 +11,14 @@ define([
 
         beforeEach(function() {
             spyOn(Backbone.history, 'start');
+            spyOn(Router.prototype, 'transition').and.callThrough();
+
             router = new Router();
+        });
+
+        afterEach(function() {
+            // remove click handler for in-site links
+            $(document).off('click');
         });
 
         describe('when constructing', function () {
@@ -47,7 +54,12 @@ define([
                 });
             });
 
+            it('should call Backbone.history.start', function () {
+                expect(Backbone.history.start).toHaveBeenCalled();
+            });
+
             it('should have a jQuery click handler on document for all in-site links', function() {
+                // $._data is an undocumented method
                 var clickEventData = _.find($._data(document, 'events').click, function (clickEventData) {
                     // TODO use a better conditional
                     // e.g. create <a href="[internal link]">, see if clickEventData.selector matches it
@@ -58,8 +70,68 @@ define([
             });
         });
 
-        // TODO test router methods
+        describe('when clicking links for each internal route', function () {
+            beforeEach(function () {
+                var routes = _.allKeys(router.routes);
 
+                routes.forEach(function (route, index, routes) {
+                    var href = '';
+                    // replace optional slash with slash, except for root link
+                    if (!route.match(/^\(\/\)$/)) {
+                        href = route.replace(/\(\/\)$/, '/');
+                    }
+
+                    $('<a href="/' + href + '" class="test-link">' + index + '</a>').appendTo('body');
+                });
+
+                spyOn(Backbone.history, 'navigate');
+            });
+
+            afterEach(function () {
+                $('a.test-link').remove();
+            });
+
+            it('should call Backbone.history.navigate for each route', function () {
+                _.each($('a.test-link'), function (link) {
+                    var urlFragment = link.href.replace(window.location.origin, '');
+
+                    $(link).click();
+                    expect(Backbone.history.navigate).toHaveBeenCalledWith(urlFragment, { trigger: true });
+                });
+            });
+        });
+
+        describe('when calling the route method home', function () {
+            beforeEach(function () {
+                router.home();
+            });
+
+            it('should call router.transitioner', function () {
+                expect(router.transition).toHaveBeenCalled();
+            });
+        });
+
+        describe('when calling the route method contact', function () {
+            beforeEach(function () {
+                router.contact();
+            });
+
+            it('should call router.transitioner', function () {
+                expect(router.transition).toHaveBeenCalled();
+            });
+        });
+
+        describe('when calling the route method staticPage', function () {
+            beforeEach(function () {
+                // set history fragment for router.staticPage method
+                Backbone.history.fragment = 'about';
+                router.staticPage();
+            });
+
+            it('should call router.transitioner', function () {
+                expect(router.transition).toHaveBeenCalled();
+            });
+        });
     });
 
 });
