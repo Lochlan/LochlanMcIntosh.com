da1d8b81fd5310a2fc50c4a57535562e22d3791e
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-05-25 22:56:02 -0700

    Fix whitespace in karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,63 @@
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    basePath: '',
+        basePath: '',
 
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine-ajax', 'jasmine', 'requirejs'],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine-ajax', 'jasmine', 'requirejs'],
+        // list of files / patterns to load in the browser
+        files: [
+            'test-main.js',
+            {pattern: 'static/src/js/**/*.js', included: false},
+            {pattern: 'tests/js/**/*Spec.js', included: false}
+        ],
 
+        // list of files to exclude
+        exclude: [
+            'static/src/js/main.js',
+        ],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'test-main.js',
-      {pattern: 'static/src/js/**/*.js', included: false},
-      {pattern: 'tests/js/**/*Spec.js', included: false}
-    ],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'static/src/js/router.js': ['coverage'],
+            'static/src/js/models/**/*.js': ['coverage'],
+            'static/src/js/templates/**/*.js': ['coverage'],
+            'static/src/js/views/**/*.js': ['coverage'],
+        },
 
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/js/'
+        },
 
-    // list of files to exclude
-    exclude: [
-      'static/src/js/main.js',
-    ],
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage'],
 
+        // web server port
+        port: 9876,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-        'static/src/js/router.js': ['coverage'],
-        'static/src/js/models/**/*.js': ['coverage'],
-        'static/src/js/templates/**/*.js': ['coverage'],
-        'static/src/js/views/**/*.js': ['coverage'],
-    },
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    coverageReporter: {
-      type : 'html',
-      dir : 'coverage/js/'
-    },
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
 
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
+    });
 };
