08bc45a731b6f1926d093f850a0abf09e9a6c9ab
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-06-07 23:33:51 -0700

    Add Backbone validation to message model, refactor message model tests

--- static/src/js/models/message.js
+++ static/src/js/models/message.js
@@ -1,14 +1,39 @@
 define([
-    'backbone',
-], function (Backbone) {
+    'models/promise_validate_model',
+], function (PromiseValidateModel) {
     'use strict';
 
-    var Message = Backbone.Model.extend({
+    var Message = PromiseValidateModel.extend({
         defaults: {
             email: '',
             name: '',
             subject: '',
             text: '',
+        },
+
+        validate: function (attributes, options) {
+
+            var errors = _.extend(
+                {},
+                _.object(
+                    // remove any implicitly returned undefined values from _.map
+                    _.compact(
+                        _.map([
+                            'email',
+                            'name',
+                            'subject',
+                            'text',
+                        ], function (field) {
+                            if (attributes[field].length === 0) {
+                                return [field, ['This field may not be blank.']];
+                            }
+                        })
+                    )
+                )
+            );
+
+            return JSON.stringify(errors) === '{}' ? undefined : errors;
+
         },
 
         urlRoot: '/api/contact/',
--- tests/js/models/messageSpec.js
+++ tests/js/models/messageSpec.js
@@ -4,23 +4,86 @@ define([
     'use strict';
 
     describe('message model', function () {
-        var message;
+        var model;
 
         beforeEach(function () {
-            message = new Message({
-                email: 'test.user@dev.lochlanmcintosh.com',
-                name: 'Test User',
-                subject: 'Good Luck',
-                text: 'I hope all your tests are passing!',
+            model = new Message();
+        });
+
+        [
+            'defaults',
+        ].forEach(function (propertyName) {
+            it('should have a property called ' + propertyName, function () {
+                expect(model[propertyName]).toBeDefined();
             });
         });
 
-        it('has default properties name, email, subject, and text', function () {
-            expect(message.defaults).toBeDefined();
-            expect(message.defaults.name).toBeDefined();
-            expect(message.defaults.email).toBeDefined();
-            expect(message.defaults.subject).toBeDefined();
-            expect(message.defaults.text).toBeDefined();
+        [
+            'validate',
+        ].forEach(function (methodName) {
+            it('should have a method called ' + methodName, function () {
+                expect(typeof model[methodName]).toEqual('function');
+            });
+        });
+
+        [
+            'email',
+            'name',
+            'subject',
+            'text',
+        ].forEach(function (propertyName) {
+            it('should have a default property called ' + propertyName, function () {
+                expect(model.defaults[propertyName]).toBeDefined();
+            });
+        });
+
+        describe('when saving', function () {
+            var onFulfilled;
+            var onRejected;
+
+            beforeEach(function () {
+                onFulfilled = jasmine.createSpy('onFulfilled');
+                onRejected = jasmine.createSpy('onRejected');
+                spyOn(model, 'validate').and.callThrough();
+
+                jasmine.Ajax.install();
+            });
+
+            afterEach(function () {
+                jasmine.Ajax.uninstall();
+            });
+
+            describe('when given valid data', function () {
+                var returnedSavedModel;
+
+                beforeEach(function () {
+                    returnedSavedModel = model.save({
+                        email: 'test.user@dev.lochlanmcintosh.com',
+                        name: 'Test User',
+                        subject: 'Good Luck',
+                        text: 'I hope all your tests are passing!',
+                    }).then(onFulfilled, onRejected);
+                });
+
+                it('should not call onRejected before the server responds', function () {
+                    expect(onRejected).not.toHaveBeenCalled();
+                });
+            });
+
+            describe('when given invalid data', function () {
+                beforeEach(function () {
+                    model.save({
+                        email: '',
+                        name: '',
+                        subject: '',
+                        text: '',
+                    }).then(onFulfilled, onRejected);
+                });
+
+                it('should call onRejected before the server responds', function () {
+                    expect(onRejected).toHaveBeenCalled();
+                });
+            });
         });
     });
 
