1a795513d6f1df213e0bc0f044345ebd93c19ad6
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-06-10 03:26:08 -0700

    Organize Django routes

--- lochlanmcintoshcom/urls.py
+++ lochlanmcintoshcom/urls.py
@@ -8,12 +8,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="shared/home.html"), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name="shared/about.html"), name='about'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('lochlanmcintoshcom.api.urls')),
     url(r'^contact/$', TemplateView.as_view(template_name="shared/contact.html"), name='contact'),
     url(r'^resume/$', TemplateView.as_view(template_name="shared/resume.html"), name='resume'),
-
-    url(r'^api/', include('lochlanmcintoshcom.api.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
 
     url(r'^browserconfig\.xml$', RedirectView.as_view(url='/static/images/favicons/browserconfig.xml')),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicons/favicon.ico')),
