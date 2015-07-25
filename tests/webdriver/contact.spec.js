5209a2952ade210bb8dc39ebdaf3432843d3e6ea
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-07-25 13:03:19 -0700

    Add contact form submission tests

--- tests/webdriver/contact.spec.js
+++ tests/webdriver/contact.spec.js
@@ -8,6 +8,7 @@ describe('Contact page', function () {
 
         browser
             .url(url)
+            .pause(10) // wait for page load
             .call(done);
     });
 
@@ -18,6 +19,45 @@ describe('Contact page', function () {
     assert.itShouldHaveTheElement('input[name="email"]');
     assert.itShouldHaveTheElement('input[name="subject"]');
     assert.itShouldHaveTheElement('textarea[name="text"]');
+
+    describe('when submitting the contact form', function (done) {
+        var pageURL;
+
+        describe('when input is valid', function (done) {
+            beforeEach(function (done) {
+                browser
+                    .url(function(err, res) {
+                        pageURL = res.value;
+                    })
+                    .setValue('input[name="name"]', 'name')
+                    .setValue('input[name="email"]', 'email@email.org')
+                    .setValue('input[name="subject"]', 'subject')
+                    .setValue('textarea[name="text"]', 'text')
+                    .submitForm('form')
+                    .pause(2000) // wait for API response
+                    .call(done);
+            });
+
+            it('Should output the success message', function (done) {
+                browser
+                    .getText('.transitioner_view-active', function(err, text) {
+                        expect(text).toEqual('Your message has been submitted successfully. Thank you!');
+                    })
+                    .call(done);
+            });
+
+            it('should not change the URL', function (done) {
+                // this assertion is ensuring the absence of a query string
+                // if the form submit listener fails then the browser will treat the form submission naively
+                browser
+                    .url(function(err, res) {
+                        var currentURL = res.value;
+                        expect(currentURL).toEqual(pageURL);
+                    })
+                    .call(done);
+            });
+        });
+    });
 
     assert.shared();
 });
