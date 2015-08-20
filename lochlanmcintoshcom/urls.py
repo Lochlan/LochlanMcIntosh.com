49596daa95dda6061e8eaa43bae976f3d5d1afb9
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-08-20 00:52:03 -0700

    Move admin and api urls

--- lochlanmcintoshcom/urls.py
+++ lochlanmcintoshcom/urls.py
@@ -8,10 +8,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="shared/home.html"), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name="shared/about.html"), name='about'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include('lochlanmcintoshcom.api.urls')),
     url(r'^contact/$', TemplateView.as_view(template_name="shared/contact.html"), name='contact'),
     url(r'^resume/$', TemplateView.as_view(template_name="shared/resume.html"), name='resume'),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('lochlanmcintoshcom.api.urls')),
 
     url(r'^browserconfig\.xml$', RedirectView.as_view(url='/static/images/favicons/browserconfig.xml')),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicons/favicon.ico')),
